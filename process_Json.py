4f4da3b88642273d132da520824d60734c7c437c
DX3906 <66631204+Eason51@users.noreply.github.com>
2021-08-10 16:11:56 +0800

    update process

--- process_Json.py
+++ process_Json.py
@@ -1,6 +1,9 @@
 import json
 
-with open('output.json','r',encoding='utf-8')as fp:
+inputName = "alkProcessed.json"
+outputName = "alkFinal.json"
+
+with open(inputName,'r',encoding='utf-8')as fp:
     json_data = json.load(fp)
 
 drug_molecule_paper = json_data["drug_molecule_paper"]
@@ -10,6 +13,77 @@ metrics_paper_count={'IC50_MC':0, 'Ki_MC':0, 'Kd_MC':0, 'Selectivity_MC':0, 'IC5
 metrics_distribution={'IC50_MC':[], 'Ki_MC':[], 'Kd_MC':[], 'Selectivity_MC':[], 'IC50_Ph':[], 'Ki_Ph':[], 'Kd_Ph':[], 'EC50_Ph':[],
                      'Selectivity_Ph':[], 'hERG_Ph':[], 'solubility_Ph':[], 'ED50_Cl':[], 'thalf_Cl':[], 'AUC_Cl':[], 'bio_Cl':[],
                      'solubility_Cl':[], 'adverse_1': [], 'adverse_2': [], 'adverse_3': []}
+
+minVitroDict = {}
+maxVitroDict = {}
+minVivoDict = {}
+maxVivoDict = {}
+
+for articleDict in drug_molecule_paper:
+    vitroDict = articleDict["pharm_metrics_vitro"]
+    for key in vitroDict:
+        if(vitroDict[key]):
+            
+            if(key in minVitroDict):
+                if(minVitroDict[key] > vitroDict[key]):
+                    minVitroDict[key] = vitroDict[key]
+            else:
+                minVitroDict[key] = vitroDict[key]
+            
+            if(key in maxVitroDict):
+                if(maxVitroDict[key] < vitroDict[key]):
+                    maxVitroDict[key] = vitroDict[key]
+            else:
+                maxVitroDict[key] = vitroDict[key]
+
+    vivoDict = articleDict["pharm_metrics_vivo"]
+    for key in vivoDict:
+        if(vivoDict[key]):
+
+            if(key in minVivoDict):
+                if(minVivoDict[key] > vivoDict[key]):
+                    minVivoDict[key] = vivoDict[key]
+            else:
+                minVivoDict[key] = vivoDict[key]
+            
+            if(key in maxVivoDict):
+                if(maxVivoDict[key] < vivoDict[key]):
+                    maxVivoDict[key] = vivoDict[key]
+            else:
+                maxVivoDict[key] = vivoDict[key]
+
+
+for articleDict in drug_molecule_paper:
+    normVitro = {}
+    vitroDict = articleDict["pharm_metrics_vitro"]
+    for key in vitroDict:
+        if(vitroDict[key]):
+            if(minVitroDict[key] == maxVitroDict[key]):
+                normVitro[key] = 0.5
+            else:
+                normVitro[key] = (vitroDict[key] - minVitroDict[key]) / (maxVitroDict[key] - minVitroDict[key])
+    
+    normVivo = {}
+    vivoDict = articleDict["pharm_metrics_vivo"]
+    for key in vivoDict:
+        if(vivoDict[key]):
+            if(minVivoDict[key] == maxVivoDict[key]):
+                normVivo[key] = 0.5
+            else:
+                normVivo[key] = (vivoDict[key] - minVivoDict[key]) / (maxVivoDict[key] - minVivoDict[key])
+    
+    articleDict["norm_pharm_metrics_vitro"] = normVitro
+    articleDict["norm_pharm_metrics_vivo"] = normVivo
+
+
+paperCountAdverse1 = set()
+paperCountAdverse2 = set()
+paperCountAdverse3 = set()
+
+partialCountAdverse1 = set()
+partialCountAdverse2 = set()
+partialCountAdverse3 = set()
+
 for i in drug_molecule_paper:
     if i['medicinal_chemistry_metrics']['IC50']!=0.0:
         metrics_paper_count['IC50_MC'] = metrics_paper_count['IC50_MC']+1
@@ -28,7 +102,7 @@ for i in drug_molecule_paper:
         metrics_distribution['IC50_Ph'].append(i['pharm_metrics_vitro']['IC50'])
     if i['pharm_metrics_vitro']['Ki']!=0.0:
         metrics_paper_count['Ki_Ph'] = metrics_paper_count['Ki_Ph']+1
-        metrics_distribution['IC50_Ph'].append(i['pharm_metrics_vitro']['IC50'])
+        metrics_distribution['Ki_Ph'].append(i['pharm_metrics_vitro']['IC50'])
     if i['pharm_metrics_vitro']['Kd']!=0.0:
         metrics_paper_count['Kd_Ph'] = metrics_paper_count['Kd_Ph']+1
         metrics_distribution['Kd_Ph'].append(i['pharm_metrics_vitro']['Kd'])
@@ -60,20 +134,50 @@ for i in drug_molecule_paper:
         metrics_paper_count['solubility_Cl'] = metrics_paper_count['solubility_Cl']+1
         metrics_distribution['solubility_Cl'].append(i['pharm_metrics_vivo']['solubility'])
     if i['clinical_statistics']:
-        metrics_paper_count['adverse_1'] = metrics_paper_count['adverse_1']+ i['clinical_statistics']['p1_company_num']
+
+
+        # metrics_paper_count['adverse_1'] = metrics_paper_count['adverse_1']+ i['clinical_statistics']['p1_company_num']
+        index = 0
+        for companyList in i['clinical_statistics']['p1_company']:
+            for companyName in companyList:
+                paperCountAdverse1.add(companyName)
+                if(index == 5 or index == 6):
+                    partialCountAdverse1.add(companyName)
+            index += 1
         for j in list(i['clinical_statistics']['p1_adverse_event'].values()):
             metrics_distribution['adverse_1'].append(j)
-        metrics_paper_count['adverse_2'] = metrics_paper_count['adverse_2'] + i['clinical_statistics']['p2_company_num']
+        # metrics_paper_count['adverse_2'] = metrics_paper_count['adverse_2'] + i['clinical_statistics']['p2_company_num']
+        index = 0
+        for companyList in i['clinical_statistics']['p2_company']:
+            for companyName in companyList:
+                paperCountAdverse2.add(companyName)
+                if(index == 5 or index == 6):
+                    partialCountAdverse2.add(companyName)
+            index += 1      
         for j in list(i['clinical_statistics']['p2_adverse_event'].values()):
             metrics_distribution['adverse_2'].append(j)
-        metrics_paper_count['adverse_3'] = metrics_paper_count['adverse_3'] + i['clinical_statistics']['p3_company_num']
+        # metrics_paper_count['adverse_3'] = metrics_paper_count['adverse_3'] + i['clinical_statistics']['p3_company_num']
+        index = 0
+        for companyList in i['clinical_statistics']['p3_company']:
+            for companyName in companyList:
+                paperCountAdverse3.add(companyName)    
+                if(index == 5 or index == 6):
+                    partialCountAdverse3.add(companyName)
+            index += 1       
         for j in list(i['clinical_statistics']['p3_adverse_event'].values()):
             metrics_distribution['adverse_3'].append(j)
 
+metrics_paper_count['adverse_1'] = len(paperCountAdverse1)
+metrics_paper_count['adverse_2'] = len(paperCountAdverse2)
+metrics_paper_count['adverse_3'] = len(paperCountAdverse3)
+
+print(f"p1 5, 6: {len(partialCountAdverse1)}")
+print(f"p2 5, 6: {len(partialCountAdverse2)}")
+print(f"p3 5, 6: {len(partialCountAdverse3)}")
 
 json_data['metrics_paper_count'] = metrics_paper_count
 json_data['metrics_distribution'] = metrics_distribution
 
-with open('processedOutput.json','w', encoding="utf-8")as fp:
+with open(outputName,'w', encoding="utf-8")as fp:
     jsonString = json.dumps(json_data, ensure_ascii=False)
     fp.write(jsonString)
