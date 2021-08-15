9538c11c60e5f059b7eb6117a5e51f8617495437
Yige Xu <xuyige1996@gmail.com>
2021-08-15 18:15:53 +0800

    1

--- nlp_implementation.py
+++ nlp_implementation.py
@@ -8,6 +8,7 @@ import fastNLP
 import torch
 
 from table import ACSTableParser, ScienceDirectTableParser
+from tener import TransformerCharEmbed, TENER
 
 
 def def_tokenizer(input_str: str):
@@ -45,6 +46,76 @@ def def_tokenizer(input_str: str):
                 final_list.append(t)
 
     return final_list
+
+
+def load_ner_model(source_vocab, target_vocab, device,
+                   file_name='/data4/chuhan/github-code/chemical-metrics/train_nlp_new/saved/model/tener_model.bin'):
+    n_heads = 12
+    head_dims = 128
+    num_layers = 2
+    lr = 0.0001
+    attn_type = 'adatrans'
+    optim_type = 'adam'
+    trans_dropout = 0.45  # 有可能是0.4
+    batch_size = 24
+
+    char_type = 'adatrans'
+
+    pos_embed = None
+
+    model_type = 'elmo'
+    warmup_steps = 0.01
+    after_norm = 1
+    fc_dropout = 0.4
+    normalize_embed = True
+
+    encoding_type = 'bio'
+    d_model = n_heads * head_dims
+    dim_feedforward = int(2 * d_model)
+
+    char_embed = TransformerCharEmbed(
+        vocab=source_vocab, embed_size=30, char_emb_size=30, word_dropout=0,
+        dropout=0.3, pool_method='max', activation='relu',
+        min_char_freq=2, requires_grad=True, include_word_start_end=False,
+        char_attn_type=attn_type, char_n_head=3, char_dim_ffn=60, char_scale=False,
+        char_dropout=0.15, char_after_norm=True
+    )
+
+    word_embed = fastNLP.embeddings.StaticEmbedding(
+        vocab=source_vocab, model_dir_or_name='en-glove-6b-100d',
+        requires_grad=True, lower=True, word_dropout=0, dropout=0.5,
+        only_norm_found_vector=True)
+
+    embed = fastNLP.embeddings.ElmoEmbedding(vocab=source_vocab, model_dir_or_name='en-original', layers='mix',
+                                             requires_grad=False, word_dropout=0.0, dropout=0.5, cache_word_reprs=False)
+
+    embed = fastNLP.embeddings.StackEmbedding([embed, word_embed, char_embed], dropout=0, word_dropout=0.02)
+
+    model = TENER(tag_vocab=target_vocab, embed=embed, num_layers=num_layers,
+                  d_model=d_model, n_head=n_heads,
+                  feedforward_dim=dim_feedforward, dropout=trans_dropout,
+                  after_norm=after_norm, attn_type=attn_type,
+                  bi_embed=None,
+                  fc_dropout=fc_dropout,
+                  pos_embed=pos_embed,
+                  scale=attn_type == 'naive')
+
+    static_dict = torch.load(file_name, map_location='cpu')
+
+    if model.transformer.layers[0].self_attn.pos_embed.weights.size() != \
+            static_dict['transformer.layers.0.self_attn.pos_embed.weights'].size():
+        model.transformer.layers[0].self_attn.pos_embed.weights = \
+            torch.randn(static_dict['transformer.layers.0.self_attn.pos_embed.weights'].size())
+    if model.transformer.layers[1].self_attn.pos_embed.weights.size() != \
+            static_dict['transformer.layers.1.self_attn.pos_embed.weights'].size():
+        model.transformer.layers[1].self_attn.pos_embed.weights = \
+            torch.randn(static_dict['transformer.layers.1.self_attn.pos_embed.weights'].size())
+
+    model.load_state_dict(static_dict)
+    model.eval()
+    model.to(device)
+
+    return model
 
 
 def load_pre_trained_nlp_model(
@@ -90,13 +161,48 @@ def load_pre_trained_nlp_model(
         headers = [s.strip() for s in lines[0].strip().split('\t')][5:]
         headers.append('compound_drug')
 
+    data_set = fastNLP.DataSet()
+
+    for data_set_name, data_set_value in final_data_set_dict.items():
+        assert isinstance(data_set_name, str)
+
+        # if 'no-ans' in data_set_name:
+        #     continue
+        if len(data_set_value) == 0:
+            continue
+
+        label_name = data_set_name[6: -8]
+
+        def get_target(instance):
+            target = ['O'] * len(instance['words'])
+            if instance['start_span_idx'] == 0 or instance['end_span_idx'] == 1:
+                return target
+            target[instance['start_span_idx']] = f'B-{label_name}'
+            target[instance['start_span_idx'] + 1: instance['end_span_idx']] = \
+                [f'I-{label_name}'] * max(instance['end_span_idx'] - instance['start_span_idx'] - 1, 0)
+            return target
+
+        assert isinstance(data_set_value, fastNLP.DataSet)
+        data_set_value.apply(get_target, new_field_name='target')
+        data_set_value.apply(lambda x: len(x['words']), new_field_name='seq_len')
+        for ins in data_set_value:
+            data_set.append(ins)
+
+    target_vocab = fastNLP.Vocabulary(unknown=None, padding=None)
+    target_vocab.from_dataset(data_set, field_name='target')
+    target_vocab.index_dataset(data_set, field_name='target', new_field_name='target')
+
+    ner_model = load_ner_model(source_vocab, target_vocab, device)
+
     result_dict = {
         'headers': headers,
         'model': model,
+        'ner_model': ner_model,
         'id_with_title_dict': id_with_title_dict,
         'source_vocab': source_vocab,
         'device': device,
-        'model_dir': model_dir
+        'model_dir': model_dir,
+        'target_vocab': target_vocab
     }
 
     return result_dict
@@ -105,7 +211,7 @@ def load_pre_trained_nlp_model(
 def get_nlp_results(table_parser: Union[ACSTableParser, ScienceDirectTableParser], **kwargs) -> dict:
     assert all([k in kwargs for k in ['headers', 'model', 'id_with_title_dict',
                                       'source_vocab', 'device',
-                                      'model_dir']])
+                                      'model_dir', 'ner_model', 'target_vocab']])
 
     def get_query_str(label: str, title: str, compound: str = '<unk>'):
         q_st = f'this paper is about {title}.' \
@@ -119,6 +225,8 @@ def get_nlp_results(table_parser: Union[ACSTableParser, ScienceDirectTableParser
 
     headers = kwargs.pop('headers')
     model = kwargs.pop('model')
+    ner_model = kwargs.pop('ner_model')
+    target_vocab = kwargs.pop('target_vocab')
     # id_with_title_dict = kwargs.pop('id_with_title_dict')
     source_vocab = kwargs.pop('source_vocab')
     # qa_data_set = kwargs.pop('qa_data_set')
@@ -174,6 +282,42 @@ def get_nlp_results(table_parser: Union[ACSTableParser, ScienceDirectTableParser
                         if content_idx >= len(tokenize_paper_content):
                             break
 
+    o_idx = target_vocab.to_index('O')
+    decode_result = {k: Counter() for k in headers}
+    for tokenize_content in tokenize_content_list:
+        last_label = None
+
+        words = torch.tensor([tokenize_content]).to('cuda:0')
+        output_logit = ner_model.predict(words)['pred'][0]
+        counts = (output_logit == o_idx).float().sum().item()
+
+        if counts != len(tokenize_content):
+            # print(f'{idx + 1}' + '=' * 20)
+            predictions = output_logit.tolist()
+            whole_prediction = []
+            for idxx, p in enumerate(predictions):
+                if p != o_idx:
+                    prediction = target_vocab.to_word(p)
+                    current_word = source_vocab.to_word(tokenize_content[idxx])
+                    assert isinstance(prediction, str) and isinstance(current_word, str)
+                    if prediction.startswith('B'):
+                        if last_label is not None:
+                            decode_result[last_label].update([' '.join(whole_prediction)])
+                        whole_prediction = [current_word]
+                        last_label = prediction[2:]
+                    else:
+                        if last_label is None or prediction[2:] != last_label:
+                            raise RuntimeError
+                        whole_prediction.append(current_word)
+                else:
+                    if len(whole_prediction) > 0:
+                        decode_result[last_label].update([' '.join(whole_prediction)])
+                    whole_prediction = []
+                    last_label = None
+
+            if last_label is not None:
+                decode_result[last_label].update([' '.join(whole_prediction)])
+
     for u_metric in headers:
         metric = u_metric.lower()
         if metric == 'compound':
@@ -216,9 +360,28 @@ def get_nlp_results(table_parser: Union[ACSTableParser, ScienceDirectTableParser
             metric_result = ''
 
         if len(metric_result) > 0:
-            result_dict[metric] = metric_result
+            decode_result[metric].update([metric_result])
 
-    return result_dict
+    for k, v in decode_result.items():
+        if len(decode_result[k]) == 0:
+            continue
+        if k == 'compound':
+            continue
+        rank_list = decode_result[k].most_common()
+        rank = 0
+        while True:
+            if rank >= len(rank_list):
+                break
+            if rank_list[rank][0] not in result_dict.values():
+                break
+            rank += 1
+        if rank < len(rank_list):
+            result_dict[k] = rank_list[rank][0]
+
+    decode_result = {k: decode_result[k].most_common(1)[0][0] for k in decode_result.keys()
+                     if len(decode_result[k]) > 0}
+
+    return {'single_dict': result_dict, 'original_dict': decode_result}
 
 
 def get_nlp_results_test(table_parser: Union[ACSTableParser, ScienceDirectTableParser], **kwargs) -> dict:
